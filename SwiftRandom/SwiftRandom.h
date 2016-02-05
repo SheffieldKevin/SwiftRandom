bd23271db202e12719c5934961f29a22da03d0df
Jonathan Wight <jwight@mac.com>
2016-02-04 16:05:11 -0800

    Initial copy of code from SwiftUtilities

---
+++ SwiftRandom/SwiftRandom.h
@@ -0,0 +1,19 @@
+//
+//  SwiftRandom.h
+//  SwiftRandom
+//
+//  Created by Jonathan Wight on 2/4/16.
+//  Copyright © 2016 schwa.io. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftRandom.
+FOUNDATION_EXPORT double SwiftRandomVersionNumber;
+
+//! Project version string for SwiftRandom.
+FOUNDATION_EXPORT const unsigned char SwiftRandomVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftRandom/PublicHeader.h>
+
+#import <SwiftRandom/CPPRandom.h>
